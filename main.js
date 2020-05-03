9e3ed15dceca20baea88a8660be4adb9d26b3231
P M <paula.melli2711@gmail.com>
2020-05-03 20:08:40 -0300

    Adds form validation
    Adds functions for client-side form validation

---
+++ main.js
@@ -0,0 +1,18 @@
+const $convertButton = document.querySelector('#convert-button');
+
+$convertButton.onclick = () => {
+    validateInput(fetchInput());
+
+};
+
+// when the user clicks convert
+    // fetching all the info
+    // validation process
+    // conversion
+
+
+const $topButton = document.querySelector('#top-button');
+// when the user clicks top
+
+
+
