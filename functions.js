318cd0bb9d8577f10fe083e10c9f7f2774d803a3
P M <paula.melli2711@gmail.com>
2020-05-04 03:24:27 -0300

    Adds API functionality
    - Data can now be fetched from the API
    - The results table can be filled with the data

--- functions.js
+++ functions.js
@@ -121,5 +121,73 @@
         return errorNumber;
     };
 
+    function summonAPI(input) {
+
+        fetch(`https://api.exchangeratesapi.io/${input[0]}?base=${input[1]}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                fillTable(myJson.rates, input);
+            });
+
+    };
+
+    function fillTable(obj, input) {
+        for (const prop in obj) {
+            const $tBody = document.querySelector('#exchange-tbody');
+            const $tr = document.createElement('tr');
+            const $currencyTh = document.createElement('th');
+            const $exchangeOne = document.createElement('td');
+
+            const currency = prop;
+            $currencyTh.innerText = currency;
+
+            const exchangeRate = obj[prop].toFixed(2);
+            $exchangeOne.innerText = exchangeRate;
+
+            $tr.appendChild($currencyTh);
+            $tr.appendChild($exchangeOne);
+
+            if (Number(input[2]) !== 1) {
+                const $exchangeAmount = document.createElement('td');
+                const exchangeForAmount = obj[prop] * Number(input[2]);
+                $exchangeAmount.innerText = exchangeForAmount.toFixed(2);
+                $tr.appendChild($exchangeAmount);
+                };
+
+
+
+            $tBody.appendChild($tr);
+        };
+
+        const inputAmount = Number(input[2]);
+
+        if (inputAmount !== 1) {
+            createAnotherColumnTitle(input);
+        };
+        
+
+    };
+
+    function createAnotherColumnTitle(input) {
+        const $tableHead = document.querySelector('#table-head')
+        const $th = document.createElement('th');
+        $th.setAttribute('scope', 'col');
+
+
+
+        const $amountDiv = document.createElement('div');
+        $amountDiv.setAttribute('id','your-amount');
+        $amountDiv.innerText = input[2];
+        $th.appendChild($amountDiv);
+
+        const $currencyDiv = document.createElement('div');
+        $currencyDiv.innerText = input[1];
+        $th.appendChild($currencyDiv);
+
+        $tableHead.appendChild($th);
+    }
+
 
 
